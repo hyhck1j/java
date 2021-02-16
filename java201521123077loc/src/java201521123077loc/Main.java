541874e25b10f9b08d7641bf2850c34cb9112835
hyhck1j <xiecong26394@126.com>
2021-02-16 22:59:37 +0800

    5-4

--- java201521123077loc/src/java201521123077loc/Main.java
+++ java201521123077loc/src/java201521123077loc/Main.java
@@ -1,26 +1,41 @@
 package java201521123077loc;
 
+
+
 import java.util.Scanner;
 
 public class Main {
 
+/*	
+ * 	数值输出保留6位小数，使用System.out.printf("%.6f\n")
+	参考Math.pow函数
+*/
+	public static double sqrt(double num)
+	{
+		final double leap=0.0001;
+		final double epsilon=0.0001;
+		double cur=0.0;
+		double ans=0.0;
+		while(ans<num&&num-ans>=epsilon)
+		{
+			cur+=leap;
+			ans=Math.pow(cur,2);
+		}
+		return cur;
+	}
 	public static void main(String[] args) {	
 		Scanner in=new Scanner(System.in);	
-		String str;
-		while(in.hasNext())
+		double num;
+		while(in.hasNextDouble())
 		{
-			str=in.nextLine();
-			str=str.replace("-","");
-			str=str.replace(".","");
-			int sum=0;
-	        String[] ss = str.split(""); 
-	        for(int i = 1; i < ss.length; i++)
-	        { 
-	            sum+= Integer.decode(ss[i]);
-	        }		    
-	        System.out.println(sum);					
+			num=in.nextDouble();
+			if(num<0)
+				System.out.println(Double.NaN);
+			else
+			{
+				System.out.printf("%.6f\n",sqrt(num));
+			}
 		}
 		in.close();
 	}
 }
-
