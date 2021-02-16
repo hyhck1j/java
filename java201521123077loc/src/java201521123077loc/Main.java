dd662e578a6f9c37216eb935138a4aebdac13990
hyhck1j <xiecong26394@126.com>
2021-02-16 22:58:15 +0800

    pta 5-2

--- java201521123077loc/src/java201521123077loc/Main.java
+++ java201521123077loc/src/java201521123077loc/Main.java
@@ -2,24 +2,42 @@ package java201521123077loc;
 
 import java.util.Scanner;
 
-
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Scanner in=new Scanner(System.in);
-		
-		int a,b;
-		
-		while(in.hasNextInt())
+	public static void main(String[] args) {	
+		Scanner in=new Scanner(System.in);	
+		String str;
+		boolean negative;
+		while(in.hasNext())
 		{
-			a=in.nextInt();
-			b=in.nextInt();
-			if(Math.abs(a)>1000)
-				System.out.println("a<=1000");
+			negative=false;
+			str=in.nextLine();
+			int num=Integer.decode(str);
+			if(num<0)
+			{
+				num*=-1;
+				negative=true;
+			}
+			if(num>=10000&&num<=20000)
+			{
+				System.out.print(Integer.toBinaryString(num)+",");
+				System.out.print(Integer.toOctalString(num)+",");
+				System.out.println(Integer.toHexString(num));
+			}
 			else
-				System.out.println(a+b);
-		}	
+			{
+		        String[] ss = str.split(""); 
+		        int first=1;
+		        int sum=0;
+		        if(negative)first=2;
+		        for(int i = first; i < ss.length; i++)
+		        { 
+		            System.out.print(ss[i]+" ");
+		            sum+= Integer.decode(ss[i]);
+		        }		    
+		        System.out.println(sum);
+			}			
+		}
 		in.close();
 	}
 
