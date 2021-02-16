420c0018f9df71f512e5b43d257fda8c2d1f3500
hyhck1j <xiecong26394@126.com>
2021-02-16 22:59:03 +0800

    pta 5-3

--- java201521123077loc/src/java201521123077loc/Main.java
+++ java201521123077loc/src/java201521123077loc/Main.java
@@ -7,38 +7,20 @@ public class Main {
 	public static void main(String[] args) {	
 		Scanner in=new Scanner(System.in);	
 		String str;
-		boolean negative;
 		while(in.hasNext())
 		{
-			negative=false;
 			str=in.nextLine();
-			int num=Integer.decode(str);
-			if(num<0)
-			{
-				num*=-1;
-				negative=true;
-			}
-			if(num>=10000&&num<=20000)
-			{
-				System.out.print(Integer.toBinaryString(num)+",");
-				System.out.print(Integer.toOctalString(num)+",");
-				System.out.println(Integer.toHexString(num));
-			}
-			else
-			{
-		        String[] ss = str.split(""); 
-		        int first=1;
-		        int sum=0;
-		        if(negative)first=2;
-		        for(int i = first; i < ss.length; i++)
-		        { 
-		            System.out.print(ss[i]+" ");
-		            sum+= Integer.decode(ss[i]);
-		        }		    
-		        System.out.println(sum);
-			}			
+			str=str.replace("-","");
+			str=str.replace(".","");
+			int sum=0;
+	        String[] ss = str.split(""); 
+	        for(int i = 1; i < ss.length; i++)
+	        { 
+	            sum+= Integer.decode(ss[i]);
+	        }		    
+	        System.out.println(sum);					
 		}
 		in.close();
 	}
-
 }
+
