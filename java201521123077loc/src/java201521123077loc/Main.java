c290ef955994388f4e41012cc88b94060f705399
hyhck1j <xiecong26394@126.com>
2021-02-21 20:33:49 +0800

    U2 5.2

--- java201521123077loc/src/java201521123077loc/Main.java
+++ java201521123077loc/src/java201521123077loc/Main.java
@@ -1,51 +1,31 @@
 package java201521123077loc;
 
-//U2 5.4
-import java.util.Arrays;
+//U2 5.2
 import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {	
-		Scanner in=new Scanner(System.in);	
-		int num;
-		int temp;
-		while(in.hasNext())
+		StringBuilder   str=new StringBuilder();
+		Scanner in=new Scanner(System.in);
+		int num=0;
+		int begin=0;
+		int end=0;
+		while(in.hasNextInt())
 		{
+			
 			num=in.nextInt();
-			String [][]arr=new String[num][];
+			begin=in.nextInt();
+			end=in.nextInt();
+			if(str.length()<num)
+			{
+				str=new StringBuilder();
+			}
 			for(int i=0;i<num;i++)
 			{
-				arr[i]=new String[i+1];
-				for(int j=0;j<=i;j++)
-				{
-					StringBuilder   str=new StringBuilder();
-					str.append(i+1);
-					str.append('*');
-					str.append(j+1);
-					str.append('=');
-					temp=(i+1)*(j+1);
-					str.append(temp);
-					arr[i][j]=str.toString();
-					
-					if(j==i)
-						System.out.println(arr[i][j]);
-					else
-					{
-						if(temp>=10)
-						{
-							System.out.print(arr[i][j]+" ");
-						}
-						else
-						{
-							System.out.print(arr[i][j]+"  ");
-						}
-					}
-						
-				}
+				str.append(i);
 			}
-			System.out.println(Arrays.deepToString(arr));
+			System.out.println(str.substring(begin,end));		
 		}
 		in.close();
 	}
 }
-
