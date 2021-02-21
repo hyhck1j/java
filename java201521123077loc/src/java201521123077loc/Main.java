19995c7feba582461c28fd9dac5a9a74421b0440
hyhck1j <xiecong26394@126.com>
2021-02-21 20:32:36 +0800

    U2 5.4

--- java201521123077loc/src/java201521123077loc/Main.java
+++ java201521123077loc/src/java201521123077loc/Main.java
@@ -1,23 +1,51 @@
 package java201521123077loc;
 
-
-import java.math.BigDecimal;
+//U2 5.4
+import java.util.Arrays;
 import java.util.Scanner;
-
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner in=new Scanner(System.in);
-		BigDecimal  first,second;
-		while(in.hasNextDouble())
+	
+	public static void main(String[] args) {	
+		Scanner in=new Scanner(System.in);	
+		int num;
+		int temp;
+		while(in.hasNext())
 		{
-			first=in.nextBigDecimal();
-			second=in.nextBigDecimal();
-			System.out.println(first.add(second));
-			System.out.println(first.multiply(second));
+			num=in.nextInt();
+			String [][]arr=new String[num][];
+			for(int i=0;i<num;i++)
+			{
+				arr[i]=new String[i+1];
+				for(int j=0;j<=i;j++)
+				{
+					StringBuilder   str=new StringBuilder();
+					str.append(i+1);
+					str.append('*');
+					str.append(j+1);
+					str.append('=');
+					temp=(i+1)*(j+1);
+					str.append(temp);
+					arr[i][j]=str.toString();
+					
+					if(j==i)
+						System.out.println(arr[i][j]);
+					else
+					{
+						if(temp>=10)
+						{
+							System.out.print(arr[i][j]+" ");
+						}
+						else
+						{
+							System.out.print(arr[i][j]+"  ");
+						}
+					}
+						
+				}
+			}
+			System.out.println(Arrays.deepToString(arr));
 		}
 		in.close();
-
 	}
-
 }
+
