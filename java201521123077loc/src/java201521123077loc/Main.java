0cd0555172618e5b15d43d64528720752bf95867
hyhck1j <xiecong26394@126.com>
2021-03-07 21:17:08 +0800

    U3 4-1

--- java201521123077loc/src/java201521123077loc/Main.java
+++ java201521123077loc/src/java201521123077loc/Main.java
@@ -1,39 +1,5 @@
-int n=sc.nextInt();
-		 Object arr[]=new Object[n];
-		 String temp;
-		 
-		 for(int i=0;i<arr.length;i++)
-		 {
-			 temp=sc.next();
-			 if(temp.equals("c"))
-			 {
-				 arr[i]=new Computer();
-			 }
-			 else if(temp.equals("d"))
-			 {
-				 double num=sc.nextDouble();
-				 arr[i]=new Double(num);
-			 }
-			 else if(temp.equals("i"))
-			 {
-				 int num=sc.nextInt();
-				 arr[i]=new Integer(num);
-			 }
-			 else if(temp.equals("s"))
-			 {
-				 String str=sc.next();
-				 arr[i]=new String(str);
-			 }
-			 else
-			 {
-				 arr[i]=null;
-			 }
-			
-		 }
-		 for(int i=arr.length-1;i>=0;i--)
-		 {
-			 if(arr[i]!=null)
-			 {
-				 System.out.println(arr[i]);
-			 }
-		 }
+public String toString()
+	{
+		return super.toString()+"-"+this.company.toString()+"-"+this.salary;
+		
+	}
