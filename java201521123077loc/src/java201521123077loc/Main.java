a3bce46ba8206bc89a542dd871e15b913d0905cb
hyhck1j <xiecong26394@126.com>
2021-03-07 21:15:27 +0800

    U3 5-6

--- java201521123077loc/src/java201521123077loc/Main.java
+++ java201521123077loc/src/java201521123077loc/Main.java
@@ -1,132 +1,348 @@
+
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
+
+
 
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-//		Scanner in = new Scanner(System.in);
-//		  
-//		 
-//		in.close();	
+		Scanner in=new Scanner(System.in);
 		
-		Company c1 = new Company("MicroSoft");
-		Company c2 = new Company("IBM");
-		Person e1 = new Employee("Li", 35, true, 60000.124, c1);
-		Person e2 = new Employee("Li", 35, true, 60000.124, c1);
-		Person e3 = new Employee("Li", 35, true, 60000.124, c2);
-		Person e4 = new Employee("Li", 35, true, 60000.123, c2);
-		Person e5 = new Employee("Li", 35, true, 60000.126, c2);
-		Person e6 = new Employee("Li", 35, true, 60000.126, null);
-//		System.out.println(e1.equals(e2)+" 1");
-		System.out.println(e2.equals(e3)+" 0");
-//		System.out.println(e1.equals(e3)+" 0");
-//		System.out.println(e3.equals(e4)+" 1");
-//		System.out.println(e3.equals(e5)+" 0");
-//		System.out.println(e5.equals(e6)+" 0");
+		String input=new String();
 		
+		ArrayList<Person> personList=new ArrayList<Person>();
 		
+		while(in.hasNext())
+		{
+			input=in.next();
+			if(input.equals("s"))
+			{
+				String name=in.next();
+				int age=in.nextInt();
+				boolean gender=in.nextBoolean();
+				String stuNo=in.next();
+				String clazz=in.next();
+				Student student=new Student(name, age, gender, stuNo, clazz);
+				personList.add(student);
+			}
+			else if(input.equals("e"))
+			{
+				String name=in.next();
+				int age=in.nextInt();
+				boolean gender=in.nextBoolean();
+				double salary = in.nextDouble();
+				String companyName=in.next();
+				Company company=null;
+				if(companyName!=null)
+				{
+					 company=new Company(companyName);
+					
+				}
+				Employee employee=new Employee(name, age, gender, company, salary);		
+				personList.add(employee);
+			}
+			else 
+			{
+				break;
+			}
+		}
+		Collections.sort( personList);
+//		System.out.println(personList);
+		for(Person itor:personList)
+		{
+			System.out.println(itor);
+		}
+		input=in.next();
+		if(input.equals("exit"))
+		{
+			in.close();
+			return ;
+		}
 		
+		ArrayList<Student> List1=new ArrayList<Student>();
+		ArrayList<Employee> List2=new ArrayList<Employee>();
+		for(Person man:personList)
+		{
+			if(man instanceof Student&&!List1.contains((Student)man))
+				List1.add((Student)man);		
+		}
+		for(Person man:personList)
+		{
+			if(man instanceof Employee&&!List2.contains(man))
+				List2.add((Employee)man);		
+		}
 		
-		
-		
+		System.out.println("stuList");
+		for(Person man:List1)
+		{
+			System.out.println(man);
+		}
+		System.out.println("empList");
+		for(Person man:List2)
+		{	
+			System.out.println(man);
+		}
+//		if(List1.get(0).equals(List1.get(1)))
+//			System.out.println("What ? same");
+//		else
+//			System.out.println("What ! different");
+		in.close();
 	}
 
 }
 
-class Person
+class Person implements Comparable<Person>
 {
-	String name;
-	int age;
-	boolean sex;
-	public Person(String name, int age, boolean sex) {
+	public String name;
+	public int age;
+	public boolean gender;
+	public Person(String name, int age, boolean gender) {
 		
 		this.name = name;
 		this.age = age;
-		this.sex = sex;
+		this.gender = gender;
+	}
+	@Override
+	public String toString() {
+		return (name+"-"+age+"-"+gender);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Person)		
+		{
+			Person man=(Person)obj;
+			if(this.age!=man.age )
+				return false;
+			if(this.gender!=man.gender)
+				return false;
+			if(this.name!=null)
+			{
+				if(man.name==null)
+					return false;
+				else if(!this.name.equals(man.name))
+					return false;
+			}
+			else 
+			{
+				if(man.name!=null)
+					return false;
+			}	
+			return true;
+		}
+			
+		return false;
+	}
+	@Override
+	public int compareTo(Person man) {
+		if(this.name==null&&man!=null)
+			return 1;
+		if(man.name==null&&this!=null)
+			return 1;
+		if(this.name==null&&man.name==null)
+			return 0;
+		if(this.name.equals(man.name))
+			return this.age-man.age;
+		else
+		{
+			return this.name.compareTo(man.name);
+		}
 	}
 	
-//	public boolean equals(Object obj)
-//	{
-//		Person temp=(Person)obj;
-//		if (obj instanceof Person) 
-//		{
-//			if()
-//				return false;
-//		}	
-//		return true;
-//	}
 }
-class Company
+class Student extends Person
 {
-	String name;
+	private String stuNo;
+	private String clazz;
+	public Student(String name, int age, boolean gender, String stuNo,
+			String clazz) {
+		super(name, age, gender);
+		this.stuNo = stuNo;
+		this.clazz = clazz;
+	}
+	
+	public String getStuNo() {
+		return stuNo;
+	}
+
+	public void setStuNo(String stuNo) {
+		this.stuNo = stuNo;
+	}
+
+	public String getClazz() {
+		return clazz;
+	}
+
+	public void setClazz(String clazz) {
+		this.clazz = clazz;
+	}
+
+	@Override
+	public String toString() {
+		
+		return "Student:"+super.toString()+"-"+this.stuNo+"-"+this.clazz;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		
+		if(obj instanceof Student)
+		{
+			Student temp=(Student)obj;
+			if(!super.equals(obj))
+			{
+				return false;
+			}
+//			System.out.println(">1");
+			if(this.stuNo!=null)
+			{
+				if(temp.stuNo==null)
+					return false;
+				if(!this.stuNo.equals(temp.stuNo))
+					return false;
+			}
+			else
+			{
+				if(temp.stuNo!=null)
+					return false;
+			}
+//			System.out.println(">2");
+			if(this.clazz!=null)
+			{
+				if(temp.clazz==null)
+					return false;
+				if(!this.clazz.equals(temp.clazz))
+					return false;
+			}
+			else
+			{
+				if(temp.clazz!=null)
+					return false;
+			}
+//			System.out.println(">3");
+			return true;
+		}
+			
+		return false;
+	}
+		
+}
+class Company 
+{
+	private String name;
+
+	
+	
 	public Company(String name) {
 		
 		this.name = name;
-	}	
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return this.name;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		
+		if(obj instanceof Company)
+		{
+			Company temp=(Company)obj;
+			
+			if(this.name!=null)
+			{
+				if(temp.name==null)
+					return false;
+				if(this.name!=temp.name)
+					return false;
+			}
+			else
+			{
+				if(temp.name!=null)
+					return false;
+			}	
+			
+			return true;
+		}		
+		return false;
+	}
+
+	
+	
 }
 class Employee extends Person
 {
-	
 	private Company company;
 	private double salary;
-	
-
-	
-	public Employee(String name, int age, boolean sex,
-			double salary, Company company) {
-		super(name, age, sex);
+	public Employee(String name, int age, boolean gender, Company company,
+			double salary) {
+		super(name, age, gender);
 		this.company = company;
 		this.salary = salary;
 	}
-
-	public boolean equals(Object obj)
-	{
-		Object tempObj= obj;
-		if (obj instanceof Employee) 
-		{
-			
-			Employee temp=(Employee) tempObj;
-			DecimalFormat df = new DecimalFormat("#.##");
+	public Company getCompany() {
+		return company;
+	}
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	@Override
+	public String toString() {
+		return "Employee:"+super.toString()+"-"+company+"-"+salary;
+	}
+	@Override
+	public boolean equals(Object obj) {
 		
+		if(obj instanceof Employee)
+		{
+			Employee temp=(Employee)obj;
+			if(!super.equals(obj))
+			{
+				return false;
+			}
+			
+			if(this.company!=null)
+			{
+				if(temp.company==null)
+					return false;
+				if(this.company.equals(temp.company))
+					return false;
+			}
+			else
+			{
+				if(temp.company!=null)
+					return false;
+			}
+			DecimalFormat df = new DecimalFormat("#.#");
 			String str1=df.format(this.salary);
 			String str2=df.format(temp.salary);
-//			System.out.println(this.salary+"<>");
 			if(!str1.equals(str2))
 				return false;
-//			System.out.println(">1");
-			if(!super.equals(obj) )
-				return false;
-//			System.out.println(">2");
-			if((this.company==null&&temp.company!=null)||(this.company!=null&&temp.company==null))
-				return false;
-//			System.out.println(">3");
-			if(this.company!=null&&temp.company!=null)
-			{
-				if(!this.company.equals(temp.company))
-				{
-					return false;
-				}
-			}
-//			System.out.println(">4");
+			
 			return true;
-		}
-//		System.out.println("Not an Employee");
+		}		
 		return false;
+			
 	}
+	
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
