c94f2a5a40e26d2984b1f0db8e0a83b24aa13c29
hyhck1j <xiecong26394@126.com>
2021-03-16 13:50:26 +0800

    U4 5-4 PairResult

--- java201521123077loc/src/java201521123077loc/Main.java
+++ java201521123077loc/src/java201521123077loc/Main.java
@@ -1,89 +1,67 @@
-import java.util.Arrays;
-import java.util.Comparator;
+package java201521123077loc;
+
 import java.util.Scanner;
-
-
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
+		
 		int n=in.nextInt();
-		PersonSortable persons[]=new PersonSortable[n];
+		double arr[]=new double [n];
+		
 		for(int i=0;i<n;i++)
 		{
-			String name=in.next();
-			int age=in.nextInt();
-			persons[i]=new PersonSortable(name, age);
+			arr[i]=in.nextDouble();
 		}
-		System.out.println("NameComparator:sort");
-		Arrays.sort(persons,new NameComparator());
-		for(int i=0;i<persons.length;i++)
-		{
-			System.out.println(persons[i]);
-		}
-		System.out.println("AgeComparator:sort");
-		Arrays.sort(persons,new AgeComparator());
-		for(int i=0;i<persons.length;i++)
-		{
-			System.out.println(persons[i]);
-		}
-		System.out.println(Arrays.toString(NameComparator.class.getInterfaces()));
-		System.out.println(Arrays.toString(AgeComparator.class.getInterfaces()));
-//		System.out.println(Arrays.toString(persons));
-//		System.out.println(Arrays.toString(PersonSortable.class.getInterfaces()));
-		in.close();
+		System.out.println(ArrayUtils.findMinMax(arr));
+		System.out.println(ArrayUtils.PairResult.class);
 	}
 
 }
-class PersonSortable 
+class ArrayUtils
 {
-	private String name;
-	private int age;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
-	public PersonSortable(String name, int age) {
-		this.name = name;
-		this.age = age;
-	}
-	@Override
-	public String toString()
+	static PairResult findMinMax(double[] values)
 	{
-		return this.name+"-"+this.age;
-	}	
-}
-
-
-class AgeComparator implements Comparator<PersonSortable> 
-{
-	@Override
-	public int compare(PersonSortable p1, PersonSortable p2) 
-	{		
-			return p1.getAge()-p2.getAge();	
+		if(values.length==0)
+		{
+			return null;
+		}
+		if(values.length==1)
+		{
+			return new PairResult(values[0],values[0]);
+		}
+		double min=values[0];
+		double max=min;
+		for(int i=1;i<values.length;i++)
+		{
+			if(values[i]>max)
+			{
+				max=values[i];
+			}
+			if(values[i]<min)
+			{
+				min=values[i];
+			}
+		}
+		return new PairResult(min, max);
 	}
-}
-
-class NameComparator implements Comparator<PersonSortable> {
-	@Override
-	public int compare(PersonSortable p1, PersonSortable p2) {
-		return p1.getName().compareTo(p2.getName());
+	static class PairResult
+	{
+		private double min;
+		private double max;
+		@Override
+		public String toString() {
+			return "PairResult [min="+min+", max="+max+"]";
+		}
+		public PairResult(double min, double max) {
+			super();
+			this.min = min;
+			this.max = max;
+		}
+		
 	}
+
+	
+	
 }
-
-
-
-
-
-
-
-
