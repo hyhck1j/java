241ae1aef5637342cf906ec9f550b7bd787025f4
hyhck1j <xiecong26394@126.com>
2021-03-28 20:59:54 +0800

    U5 5-3 (treeMap)

--- java201521123077loc/src/u5Collections/Main.java
+++ java201521123077loc/src/u5Collections/Main.java
@@ -1,31 +1,56 @@
 package u5Collections;
 
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.TreeMap;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner in=new Scanner(System.in);
+				
+		TreeMap<String, Integer> map = new TreeMap<String, Integer>();
 		
-		TreeSet<String> set=new TreeSet<String>();
 		while(in.hasNext())
 		{
 			String str=in.next();
 			if(str.equals("!!!!!"))
 				break;
-			set.add(str);
+			Integer count=map.get(str);
+			if(count!=null)
+			{
+				map.put(str,count+1 );
+			}
+			else
+				map.put(str, 1);
 		}
-		System.out.println(set.size());
-		Iterator<String> it=set.iterator();
-		String str=new String();
-		for(int i=0;i<10;i++)
-		{
-			str=set.pollFirst();
-			if(str!=null)
-				System.out.println(str);
-		}
+		System.out.println(map.size());
+		List<Map.Entry<String,Integer>> list=new ArrayList<Map.Entry<String,Integer>>(map.entrySet()) ;
+		
+         Collections.sort(list,new Comparator<Map.Entry<String,Integer>>() {
+
+			@Override
+			public int compare(Entry<String, Integer> o1,
+					Entry<String, Integer> o2) {
+				if(o1.getValue().equals(o2.getValue()))
+				{
+					return o1.getKey().compareTo(o2.getKey());
+				}
+				return o2.getValue().compareTo(o1.getValue());
+			}
+		});
+         int count=0;
+         for(Entry<String, Integer> mapping:list){ 
+        	 if(count==10)
+        		 break;
+             System.out.println(mapping.getKey()+"="+mapping.getValue()); 
+             count++;
+        } 
 		in.close();
 	}
 
