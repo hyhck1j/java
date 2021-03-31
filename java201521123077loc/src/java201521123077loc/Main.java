6a818666dfaf4c285748c275229e81d718fd3d94
hyhck1j <xiecong26394@126.com>
2021-03-31 20:19:08 +0800

    U5 4-1 two simple method

--- java201521123077loc/src/java201521123077loc/Main.java
+++ java201521123077loc/src/java201521123077loc/Main.java
@@ -1,67 +1,40 @@
 package java201521123077loc;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner in=new Scanner(System.in);
-		
-		int n=in.nextInt();
-		double arr[]=new double [n];
-		
-		for(int i=0;i<n;i++)
-		{
-			arr[i]=in.nextDouble();
-		}
-		System.out.println(ArrayUtils.findMinMax(arr));
-		System.out.println(ArrayUtils.PairResult.class);
+	/*remove函数代码*/
+	private static void remove(List<String> list, String word) {
+		while(list.contains(word))
+			list.remove(word);		
 	}
-
-}
-class ArrayUtils
-{
-	static PairResult findMinMax(double[] values)
-	{
-		if(values.length==0)
-		{
-			return null;
-		}
-		if(values.length==1)
-		{
-			return new PairResult(values[0],values[0]);
-		}
-		double min=values[0];
-		double max=min;
-		for(int i=1;i<values.length;i++)
-		{
-			if(values[i]>max)
-			{
-				max=values[i];
-			}
-			if(values[i]<min)
-			{
-				min=values[i];
-			}
-		}
-		return new PairResult(min, max);
-	}
-	static class PairResult
-	{
-		private double min;
-		private double max;
-		@Override
-		public String toString() {
-			return "PairResult [min="+min+", max="+max+"]";
-		}
-		public PairResult(double min, double max) {
-			super();
-			this.min = min;
-			this.max = max;
-		}
+	 /*covnertStringToList函数代码*/
+	private static List<String> convertStringToList(String nextLine) {
+		List<String> list=new ArrayList<String>();
 		
+		Scanner in=new Scanner(nextLine);
+		while(in.hasNext())
+		{
+			list.add(in.next());
+		}
+		in.close();
+		return list;
 	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextLine()){
+            List<String> list = convertStringToList(sc.nextLine());
+            System.out.println(list);
+            String word = sc.nextLine();
+            remove(list,word);
+            System.out.println(list);
+        }
+        sc.close();
+    }
 
 	
-	
+
 }
